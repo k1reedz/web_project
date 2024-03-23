5e9a1085e2ac98eec87f669b49738b4000d24aeb
E7425 <egoroleshko555@gmail.com>
2024-03-23 23:06:17 +0300

    Preparations and loging
    Подготовка основной структуры проекта. Реализована регистрации. Для реализации логина не хватает шаблона для отображения формы

--- main_page/main.py
+++ main_page/main.py
@@ -1,8 +1,17 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request, abort, make_response, jsonify
+from data import db_session
+from data.users import User
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from forms.user_form import RegisterForm, LoginForm
+
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
+# Главная страница
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -13,7 +22,67 @@ def base():
     return render_template("base.html")
 
 
+# Обработчик регистрации
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            # пока используется временный шаблон
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пользователь с таким email уже существует")
+        user = User(
+            surname=form.surname.data,
+            name=form.name.data,
+            date_of_birth=form.date_of_birth.data,
+            email=form.email.data,
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        # Форма логина еще не готова
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+@app.errorhandler(400)
+def bad_request(_):
+    return make_response(jsonify({'error': 'Bad Request'}), 400)
+
+
 def main():
+    db_session.global_init("db/users.db")
     app.run()
 
 
