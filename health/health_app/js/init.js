65b0438ceceeb242517c75a7257895ef7a7cdc6d
rammsteeein <151989069+rammsteeein@users.noreply.github.com>
2024-03-31 08:55:45 +0300

    Add files via upload

---
+++ health/health_app/js/init.js
@@ -0,0 +1,10 @@
+(function($){
+  $(function(){
+    // Инициализация боковой навигации
+    $('.sidenav').sidenav();
+
+    // Инициализация tooltips
+    $('.tooltipped').tooltip();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
